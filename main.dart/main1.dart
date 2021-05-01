4e1eb805604dcda132ab3b7c2461b04e84378b04
MudassirRaza12345 <mudassirraza12345@gmail.com>
2021-05-01 21:02:02 +0500

    first commit

---
+++ main.dart/main1.dart
@@ -0,0 +1,124 @@
+void main() {
+  
+//    var list1 = ["bilal",1,true,"ali","umair"];
+//   var list2 = [2,6,4,7,9,0];
+//   var list3 = ["bilal","ali","umair","huzaifa"];
+
+  
+//   list1.replaceRange(2,4,["ahmed",false]);
+//   list3.sort();
+// //   print(list1);
+//   print(list3);
+  
+  
+//       list1[3]="ali";
+// int a = 3;
+// String b = "bilal";
+// double c = 2.97;
+// bool d = true;
+//   list3.sort();
+//   print(list1);
+  
+//   var list5 = [];
+//   if(list5.isEmpty){
+//   print("Empty list found");
+//   }
+  
+//     list5.add("ali");
+//   list5.add("bilal");
+//   print(list5);
+  
+//    if(list5.isNotEmpty){
+// print("List is not empty");
+//   }
+//   list5.clear();
+//   print(list5);
+  
+  
+//   var list6=[8,4,6,9,7,1];
+// //   var x = List.of(list6.reversed);
+// //   print(x);
+  
+  
+//   var list7 = [12,19,-7,-2,-1,25,36];
+//   list6.addAll(list7);
+//   print(list6);
+//   list6.sort();
+//   print(list6);
+  
+//   list6.insert(4,50);
+//   print(list6);
+  
+  
+  
+  
+  
+//   var list8 = ["ali","bilal"];
+//   list8.insertAll(1,["huzaifa","umair","ghous"]);
+// //   print(list8);
+  
+  
+  
+// //   list8.remove("ghous");
+// //   print(list8);
+  
+  
+  
+// //     list8.removeAt(0);
+// //   print(list8);
+  
+// //   list7.removeWhere((e) => e > 0);
+// //   print(list7);
+  
+  
+  
+//   list8.removeRange(2, 3);
+// //   print(list8);
+  
+  
+//   list7.retainWhere((e) => e > 7);
+//   print(list7);
+// }
+  
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//    print(map1.length);
+  
+  
+//    var map1 = {1:'bilal',2:'umair',4:'ali'};
+//   map1[5] = "ahmed";
+//   print(map1);
+  
+//    var map1 = {"1":'bilal',2:'umair',4:'ali'};
+//   map1.addAll({6:"abc",7:"fff"});
+//   print(map1);
+  
+//   var mylist = [0,'ali','owais'];
+//   mylist.add('bilal');
+//   print(mylist);
+  
+//   var mylist = [5,3,2,4,1,0];
+//   mylist.remove(3);
+//   print(mylist);
+//   var abc = jawanpakistan; 
+//   print(abc);
+  
+//    var num = 25; 
+//   if(num>10){
+//     print('true');
+//   }else
+//   {
+//     print('false'); 
+    
+  
+  
+// }
+  
+  
+  
+  
+  if(false){ print('true'); }else{ print('false'); }
+  
+
+}
+  
