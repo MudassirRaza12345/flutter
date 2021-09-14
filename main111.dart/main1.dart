d84314c083e3b1dcf1bc1dd69d78ef86d5141c76
MUHAMMAD MUDASSIR RAZA <57351981+MudassirRaza12345@users.noreply.github.com>
2021-09-14 02:35:33 -0700

    Update main1.dart

--- main111.dart/main1.dart
+++ main111.dart/main1.dart
@@ -199,6 +199,184 @@ list3.sort();
   
 // O:false  
   
+  void main() {
+   
+//    var list1 = ["bilal",1,true,"ali","umair"];
+//   var list2 = [2,6,4,7,9,0];
+//   var list3 = ["bilal","ali","umair","huzaifa"];
+
+  
+//   list1.replaceRange(2,4,["ahmed",false]);
+ 
+  
+  //   list1[3]="huzaifa";
+// int a = 3;
+// String b = "bilal";
+// double c = 2.97;
+// bool d = true;
+//   list3.sort();
+//   print(list1);
+  
+//   var list5 = [];
+//   if(list5.isEmpty){
+//   print("Empty list found");
+//   }
+  
+//   list5.add("ali");
+//   list5.add("bilal");
+//   print(list5);
+//   if(list5.isNotEmpty){
+// print("List is not empty");
+//   }
+//   list5.clear();
+//   print(list5);
+  
+//   var list6=[8,4,6,9,7,1];
+//   var x = List.of(list6.reversed);
+//   print(x);
+// O:  [1, 7, 9, 6, 4, 8]
+  
+//   var list7 = [12,19,-7,-2,-1,25,36];
+//   list6.addAll(list7);
+//   print(list6);   
+//   O:[8, 4, 6, 9, 7, 1, 12, 19, -7, -2, -1, 25, 36]
+//   list6.sort();
+//   print(list6);
+//   O: [-7, -2, -1, 1, 4, 6, 7, 8, 9, 12, 19, 25, 36]
+  
+//   var list6=[8,4,6,9,7,1];
+//   list6.insert(4,50);
+//   print(list6);
+//  O: [8, 4, 6, 9, 50, 7, 1]
+  
+//   var list8 = ["ali","bilal"];
+//   list8.insertAll(1,["huzaifa","umair","ghous"]);
+//   print(list8);
+//   O:[ali, huzaifa, umair, ghous, bilal]
+  
+//   list8.remove("ghous");
+//   print(list8);
+// //   O:[ali, huzaifa, umair, bilal]
+  
+//   list8.removeAt(0);
+//   print(list8);
+//   O:[huzaifa, umair, ghous, bilal]
+  
+//   var list7 = [12,19,-7,-2,-1,25,36];
+//   list7.removeWhere((e) => e > 0);
+//   print(list7);
+  //O:[-7, -2, -1]
+  
+//   var list8 = ["ali", "huzaifa", "umair"," ghous", "bilal"];
+//   list8.removeRange(2, 3);
+//   print(list8);
+//   O:[ali, huzaifa,  ghous, bilal]
+  
+//   var list7 = [12,19,-7,-2,-1,25,36];
+//   list7.retainWhere((e) => e > 7);
+//   print(list7);
+//   O:[12, 19, 25, 36]
+//   Dictionary
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//   print(map1.length);
+//   O:3
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//   map1[5] = "ahmed";
+//   print(map1);
+//   O:{1: bilal, 2: umair, 4: ali, 5: ahmed}
+  
+  
+//   var map1 = {"1":'bilal',2:'umair',4:'ali'};
+//   map1.addAll({6:"abc",7:"fff"});
+//    print(map1);
+// //  O:{1: bilal, 2: umair, 4: ali, 6: abc, 7: fff}
+  
+  
+//   var students = {'12a':["bilal",20,"b"],"13a":["umair",22,"a"]};
+//   print(students);
+//  O: {12a: [bilal, 20, b], 13a: [umair, 22, a]}
+  
+  
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//   map1.remove(3);
+//   print(map1);
+//   O:{1: bilal, 2: umair, 4: ali}
+  
+  
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//   map1.removeWhere((key, value) => value.startsWith('u'));
+//   print(map1);
+//   {1: bilal, 4: ali}
+  
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//   var map2 = {3:'abc',5:'aaa'};
+//   var mapp = {}..addAll(map1)..addAll(map2);
+//   print(mapp);
+// O:  {1: bilal, 2: umair, 4: ali, 3: abc, 5: aaa}
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//    var map2 = {3:'abc',5:'aaa'};
+//   var mapp = {...map1,...map2};
+//   print(mapp);
+//   O:{1: bilal, 2: umair, 4: ali, 3: abc, 5: aaa}
+  
+//   var lst1 = [1,2,3,4,5];
+//   var lst2 = ["bilal","umair","ali","aaa","bbb"];
+//   var mapp = Map.fromIterables(lst1,lst2);
+//   print(mapp);
+//   o:{1: bilal, 2: umair, 3: ali, 4: aaa, 5: bbb}
+  
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//   var abc = map1.containsKey(6);
+//   print(abc);
+  //   O:false
+  
+  
+//   var map1 = {1:'bilal',2:'umair',4:'ali'};
+//   var abc = map1.containsValue("abccc");
+//   print(abc);
+//   O:false
+  
+  
+//   Applies the specified function on every Map entry. 
+//     In other words, forEach enables iterating through the Map’s entries.
+     
+//      var map1 = {1:'bilal',2:'umair',4:'ali'};
+//       map1.forEach((key,value){
+//         print('Keys are: ${key} and Values are :${value}');
+//       }
+      
+//       );
+  //  O: Keys are: 1 and Values are :bilal
+//     Keys are: 2 and Values are :umair
+//    Keys are: 4 and Values are :ali
+  
+  
+  
+//   var abc = "bilal";
+//   print('${abc}');
+//   o:bilal
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+  
   //--------Some Practice work------------------   
   
   
